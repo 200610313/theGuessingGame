919503d2fdd8966d335aad663e0de0bb1a541714
200610313 <41291228+200610313@users.noreply.github.com>
2019-06-19 23:29:44 +0800

    Increased the level of Abstraction.

--- guessNum.java
+++ guessNum.java
@@ -2,55 +2,93 @@
 Angelu Ferdinand Garcia
 Object Oriented Programming 2
 Mr. Jessie Lagrosas
-6/17/19
+6/19/19
 
 User guesses a randomly-generated integer.
 Program gives a hint/tip if guess is lesser or greater
 */
-import java.util.Scanner;
 import java.util.Random;
-public class guessNum
+import java.util.Scanner;
+
+public class launcher{
+    public static void main(String[] args) {
+        guessNum g = new guessNum();
+        g.setLimit();
+        g.guess();
+    }
+}
+
+
+class guessNum
 {
-		int limit; //upper limit
-		int guess; //user guess
-		int trueNum; //random num
+    private Scanner in = null;      //object to get input
+    private Random rand = null;     //object to generate random num
+    private int limit;              //upper limit
+    private int guess;              //user guess
+    private int trueNum;            //random num
+    private int firstGuess;
 
-	public static void main(String[] args) {
-		//to access instance variables
-		guessNum g = new guessNum();
-		//to get user input
-		Scanner in = new Scanner(System.in);
-		//let user choose limit
-		System.out.print("Enter upper limit: ");g.limit = in.nextInt();	
-		//generate random number
-		Random rand = new Random();g.trueNum = rand.nextInt(g.limit+1);
+    //constructor
+    public guessNum()
+    {
+        in = new Scanner(System.in);
+        rand = new Random();
+        this.limit = 0;
+        this.guess = 0;
+        this.trueNum = 0;
+    }
 
-		System.out.println("0 to "+g.limit+". Start guessing! \nGuess: ");
-		
-		//let user make 1st guess
-		int firstGuess = in.nextInt();
+    public void setLimit()
+    {
+        System.out.print("Enter upper limit: ");
+        //setting upper limit, store it to limit
+        this.limit = this.in.nextInt();
+        //generate random number
+        setRandom();
+    }
 
-		if (firstGuess!=g.trueNum) {
-			g.guess = firstGuess;
-			while((wrongGuess(g.guess, g.trueNum))){
-				giveTip(g.guess, g.trueNum);
-				System.out.print("Guess: ");
-				g.guess = in.nextInt();
-			}
-			System.out.println("Correct! the number was "+g.trueNum+".");
-		}
-		else System.out.println("Correct! the number was "+g.trueNum+". You guessed the number on your 1st try.");
+    private void setRandom()//private because never casted in main
+    {
+        //generate a random variable, store it to trueNum
+        this.trueNum = this.rand.nextInt(this.limit+1);
+    }
 
-	}
+    public void guess()
+    {
+        System.out.print("Guess the number from 0 to "+this.limit+"." +
+                         "\nGuess: ");
 
-	private static boolean wrongGuess(int guess, int trueNum ){
-		if (guess == trueNum) return false;
-		else return true;
-	}
+        //loop will only activate if first guess is wrong, make first guess.
+        this.firstGuess = this.in.nextInt();
+        //if user made the wrong guess on first try
+        if (this.firstGuess!=this.trueNum)
+        {
+            this.guess = this.firstGuess;//encountered only once
 
-	private static void giveTip(int guess, int trueNum)
-	{
-		if (guess>trueNum) System.out.println("Go lower.");
-		else System.out.println("Go higher.");
-	}
+            while(wrongGuess())
+            {
+                //tip user if true number was higher/lower than guess
+                giveTip();
+                System.out.print("Guess: ");
+                this.guess = this.in.nextInt();
+            }
+            //if guess finally right, let user know
+            System.out.println("Correct! the number was "+this.trueNum+".");
+        }
+        //if user made the right guess on the first try
+        else System.out.println("Correct! the number was "+this.trueNum+". You guessed the number on your 1st try.");
+    }
+
+    private boolean wrongGuess()
+    {
+        //check if guess is correct
+        if (this.guess == this.trueNum) return false;
+        else return true;
+    }
+
+    private void giveTip()
+    {
+        if (this.guess>this.trueNum) System.out.println("Go lower.");
+        else System.out.println("Go higher.");
+    }
 }
